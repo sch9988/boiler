6da10cfe00118b5e4bea648abc81f56c7d5d2e86
재이 <jay@jaeiui-MacBookAir.local>
2021-11-05 17:08:05 +0900

    처음 저장소에 올림

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const bodyParser = require('body-parser')
+
+const config = require('./config/key')
+
+const {User} = require('./models/User')
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+const mongoose = require('mongoose')
+mongoose.connect(config.mongoURI, {
+
+}).then(() => console.log('MongoDB connected...'))
+.catch(error => console.log(error))
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World! Nodejs!')
+})
+
+app.post('/register',(req,res)=>{
+    const user = new User(req.body)
+    user.save((err,userInfo)=>{
+        if(err) return res.json({success:false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
